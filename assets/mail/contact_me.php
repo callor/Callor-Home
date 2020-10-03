3cf1df55ed566928f723073470f8459e3ed536ec
callor <callor@callor.com>
2020-10-03 17:27:50 +0900

    php

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -16,11 +16,11 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'callor@daum.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'callor@callor.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@callor.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
+mail($to,$email_subject,$email_body);//,$headers);
 return true;         
 ?>
