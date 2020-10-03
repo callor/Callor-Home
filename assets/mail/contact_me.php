5e2bc47fcb44b17da657218ec1a722e25f1973e9
callor <callor@callor.com>
2020-10-03 18:56:30 +0900

    mail

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -21,6 +21,7 @@ $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From:callor@daum.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers,"-fcallor@callor.com");
+echo mail($to,$email_subject,$email_body,$headers,"-fcallor@callor.com");
+
 return true;         
 ?>
