80e825aabc888d7fa719ba62dbb12f8eaa76a390
callor <callor@callor.com>
2020-10-03 19:11:42 +0900

    mail

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -1,17 +1,17 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
+// if(empty($_POST['name'])      ||
+//    empty($_POST['email'])     ||
+//    empty($_POST['phone'])     ||
+//    empty($_POST['message'])   ||
+//    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+//    {
+//    echo "No arguments Provided!";
+//    return false;
+//    }
    
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$name = "홍길동"; //strip_tags(htmlspecialchars($_POST['name']));
+$email_address = "callor88@naver.com"; // strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
