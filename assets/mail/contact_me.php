54338dc6af0118453908a513113cd027672376f6
callor <callor@callor.com>
2020-10-03 21:29:34 +0900

    mail

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -17,15 +17,15 @@ if(empty($_POST['name'])      ||
 
 
 
-$name = "홍길동"; //strip_tags(htmlspecialchars($_POST['name']));
-$email_address = "callor88@naver.com"; // strip_tags(htmlspecialchars($_POST['email']));
-$phone = "0101010101" ; // strip_tags(htmlspecialchars($_POST['phone']));
-$message = "Hello" ; //strip_tags(htmlspecialchars($_POST['message']));
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
 $to = 'callor88@naver.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "현재 시간 : ". date("H:i:s")."<br/>"; // "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body =  "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 // $headers = "From: webmaster<callor@callor.com>\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 // $headers .= "Reply-To: $email_address";   
 
