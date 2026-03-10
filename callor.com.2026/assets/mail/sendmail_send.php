e98a6b1a4e4ca2bb58c90a5e96ded916f08dc368
callor <callor@callor.com>
2026-03-10 15:12:54 +0900

    from

--- callor.com.2026/assets/mail/sendmail_send.php
+++ callor.com.2026/assets/mail/sendmail_send.php
@@ -119,7 +119,7 @@ $body = "
 $fromName = mb_encode_mimeheader($userName, "UTF-8", "B");
 
 // $headers  = "From: {$fromName} <{$userEmail}>\r\n";
-$headers  = "From: callor.com <callor@callor.com>\n";
+$headers  = "From: callor.com <callor@callor.com>\r\n";
 $headers .= "Reply-To: {$userName} <{$userEmail}>\n";
 $headers .= "MIME-Version: 1.0\n";
 $headers .= "Content-Type: text/html; charset=UTF-8\n";
